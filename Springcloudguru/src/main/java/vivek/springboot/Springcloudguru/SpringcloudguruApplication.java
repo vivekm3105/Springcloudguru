adef497e62bff67ad9a9bd36bd19cd0e3b92ee7d
mvivek3105 <vmvby@mail.umkc.edu>
2020-10-10 13:52:02 +0530

    project setup

---
+++ Springcloudguru/src/main/java/vivek/springboot/Springcloudguru/SpringcloudguruApplication.java
@@ -0,0 +1,13 @@
+package vivek.springboot.Springcloudguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudguruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudguruApplication.class, args);
+	}
+
+}
---
+++ Springcloudguru/src/test/java/vivek/springboot/Springcloudguru/SpringcloudguruApplicationTests.java
@@ -0,0 +1,13 @@
+package vivek.springboot.Springcloudguru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudguruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
